4c4b66ab9124a579be67f83ec7af73292424842d
panos <p.vakalopoulos@vidaxl.com>
2019-06-19 15:46:07 +0200

    - polished header buttons

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -12,20 +12,23 @@
             class="display-inline header-button mr-m"
             @click="goToPage('orders')"
           >
-            Manage Orders
+            <i class="material-icons">store</i>
+            <span>Orders</span>
           </button>
           <button
             v-if="isMasterJedi"
             class="display-inline header-button mr-m"
             @click="goToPage(buttonData.routeName)"
           >
-            {{ buttonData.text }}
+            <i class="material-icons">build</i>
+            <span>{{ buttonData.text }}</span>
           </button>
           <button
             class="display-inline header-button"
             @click="goToPage('login')"
           >
-            Logout
+            <i class="material-icons">exit_to_app</i>
+            <span>Logout</span>
           </button>
         </section>
       </div>
@@ -43,7 +46,7 @@ export default {
     ...mapState('account', ['isMasterJedi']),
     buttonData () {
       return {
-        text: !this.$route.name || this.$route.name === 'home' ? 'Manage Sabers' : 'Go back',
+        text: !this.$route.name || this.$route.name === 'home' ? 'Sabers' : 'Go back',
         routeName: !this.$route.name || this.$route.name === 'home' ? 'manager' : 'home'
       }
     }
